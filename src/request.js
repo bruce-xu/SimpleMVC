d737264533632429c44a5ec62e5705f538d71c16
徐玉建 <xuyujian376@c07px0zeg1j1.ljs.local>
2017-09-05 21:19:16 +0800

    Update

--- src/request.js
+++ src/request.js
@@ -4,7 +4,31 @@
  */
 
 define(function (require) {
-  function request() {
+  function request(options) {
+  	var httpRequest;
+
+  	if (window.XMLHttpRequest) {
+  		httpRequest = new XMLHttpRequest();
+  	} else if (window.ActiveXObject) {
+  		httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+
+  	httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  	httpRequest.onreadystatechange = onLoad;
+    httpRequest.open('GET', 'test.html');
+    httpRequest.send();
+  }
+
+  function onLoad(httpRequest) {
+  	if (httpRequest.readyState === 4) {
+    	if (httpRequest.status === 200) {
+			  
+			}
+		}
+  }
+
+  function get(options) {
 
   }
 
